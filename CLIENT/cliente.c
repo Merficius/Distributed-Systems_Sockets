2d4ab97a0deb37a2552c5df6c47d209572275494
Mariano Portilla <44553694+Merficius@users.noreply.github.com>
2021-09-06 20:01:40 -0500

    Add character sending and integer sending using big-endian

--- CLIENT/cliente.c
+++ CLIENT/cliente.c
@@ -6,14 +6,35 @@
 #include <string.h>
 #include <sys/socket.h>
 
+char c1 = 'A'; // 30
+char c2 = 'B';
+int i = 16; // Entero a 32 bits representado en LITTLE-ENDIAN
+char c3 = 'C';
+char c4 = 'D';
+
 void send_integer(int sock);
+void send_character(int sock, char* character);
 
 int main(int argc, char *argv[])
 {
     int sock = connection();
     
     // communicating with server
-    send_integer(sock);
+    
+    // Envío de primeros dos caracteres
+    send_character(sock, &c1);
+    send_character(sock, &c2);
+    
+    // Envío de entero dividido en 4 bytes usando caracteres, ordenado en big-endian
+    char *p = (char *)&i;
+    send_character(sock, &p[3]);
+    send_character(sock, &p[2]);
+    send_character(sock, &p[1]);
+    send_character(sock, &p[0]);
+    
+    // Envío de últimos dos caracteres
+    send_character(sock, &c3);
+    send_character(sock, &c4);
     
     close(sock);
     return 0;
@@ -29,6 +50,14 @@ void send_integer(int sock)
     
     // send some data
     if(send(sock, &integer, sizeof(integer), 0) < 0) {
+        puts("Send failed");
+        exit(1);
+    }
+}
+
+void send_character(int sock, char* character) //30
+{
+    if( send(sock, character, sizeof(*character), 0) < 0) {
         puts("Send failed");
         exit(1);
     }
--- SERVER/server.c
+++ SERVER/server.c
@@ -6,7 +6,16 @@
 #include <unistd.h>
 #include <sys/socket.h>
 
+// Variables globales
+char c1 = 'U';
+char c2 = 'U';
+// entero a 64 bits representado en LITTLE-ENDIAN
+long i = -1;
+char c3 = 'U';
+char c4 = 'U';
+
 int recv_integer(int client_sock);
+char recv_character(int client_sock);
 
 int main(int argc, char *argv[])
 {
@@ -16,8 +25,38 @@ int main(int argc, char *argv[])
         
         int client_sock = connection(socket_desc);
         
-        int integer = recv_integer(client_sock);
-        printf("%d\n", integer);
+        // Recuperación primeros dos caracteres
+        char c1 = recv_character(client_sock);
+        printf("%c\n", c1);
+        char c2 = recv_character(client_sock);
+        printf("%c\n", c2);
+
+        // Lectura de caracteres enviados (representados en big-endian) para los primeros 4 bytes del long (representado en little-endian)
+        char c3 = recv_character(client_sock);
+        char c4 = recv_character(client_sock);
+        char c5 = recv_character(client_sock);
+        char c6 = recv_character(client_sock);
+        
+        // Creación de variable long y puntero para asignar valores
+        long i;
+        char *p = (char *)&i;
+        
+        // Asignación de valores para el long representado en little-endian
+        p[3] = c3;
+        p[2] = c4;
+        p[1] = c5;
+        p[0] = c6;
+        p[4] = 0;
+        p[5] = 0;
+        p[6] = 0;
+        p[7] = 0;
+        printf("%ld\n", i);
+        
+        // Recuperación de últimos dos caracteres
+        char c7 = recv_character(client_sock);
+        printf("%c\n", c7);
+        char c8 = recv_character(client_sock);
+        printf("%c\n", c8);
     }
     
     close(socket_desc);
@@ -40,4 +79,23 @@ int recv_integer(int client_sock)
     }
     
     return integer;
+}
+
+char recv_character(int client_sock)
+{
+    int read_size;
+    char character;
+    
+    // receive a message from client
+    read_size = recv(client_sock, &character, sizeof(character), 0);
+    
+    if(read_size == 0) {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1) {
+        perror("recv failed");
+    }
+    
+    return character;
 }
